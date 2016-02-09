42581e5218cba3c8f15b0d762f0dc1724bf61ebb
Anna Lawley and Blair Peterson <alawley1@gmail.com>
2016-02-09 08:44:45 -0800

    add values for rest of letters

--- src/main/java/ScrabbleScore.java
+++ src/main/java/ScrabbleScore.java
@@ -9,6 +9,31 @@ public class ScrabbleScore {
     char[] letters = word.toCharArray();
     HashMap<Character, Integer> values = new HashMap<Character, Integer>(){{
       put('a', 1);
+      put('e', 1);
+      put('i', 1);
+      put('o', 1);
+      put('u', 1);
+      put('l', 1);
+      put('n', 1);
+      put('r', 1);
+      put('s', 1);
+      put('t', 1);
+      put('d', 2);
+      put('g', 2);
+      put('b', 3);
+      put('c', 3);
+      put('m', 3);
+      put('p', 3);
+      put('f', 4);
+      put('h', 4);
+      put('v', 4);
+      put('w', 4);
+      put('y', 4);
+      put('k', 5);
+      put('j', 8);
+      put('x', 8);
+      put('q', 10);
+      put('z', 10);
     }};
 
     for (char letter : letters) {
--- src/test/java/ScrabbleScoreTest.java
+++ src/test/java/ScrabbleScoreTest.java
@@ -9,4 +9,16 @@ public class ScrabbleScoreTest {
     Integer score = 1;
     assertEquals(score, newScore.scoreCalculate("a"));
   }
+  @Test
+  public void scrabbleScore_returnsAScrabbleScoreForAWord_8() {
+    ScrabbleScore newScore = new ScrabbleScore();
+    Integer score =  8 ;
+    assertEquals(score, newScore.scoreCalculate("hello"));
+  }
+  @Test
+  public void scrabbleScore_returnsAScrabbleScoreForAllLetters_87() {
+    ScrabbleScore newScore = new ScrabbleScore();
+    Integer score = 87;
+    assertEquals(score, newScore.scoreCalculate("abcdefghijklmnopqrstuvwxyz"));
+  }
 }
